25ec9e474e24a3ca45905e0ab8478205e986fb62
Michael Fuget <michaelfuget@gmail.com>
2020-12-09 07:39:29 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ("Mr. Chocolate", 2, "M", "light brown", "nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ("Rowdy", 2, "M", "dark brown", "nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ("Tabitha", 6, "M", "light brown", "nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ("Sergeant Brown", 2, "M", "black", "nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ("Melissa", 13, "M", "dark brown", "nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ("Grinch", 2, "M", "polar", "nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ("Wendy", 6, "M", "dark brown", "nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES (NULL, 20, "M", "black", "agressive", 0);
